0c6b8261bee53865d539897da5aca54144c686b7
AnushAdk04 <adhikarianush64@gmail.com>
2025-12-03 09:00:32 +0545

    student file added and props explained

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Card from "./Card";
 import Footer from "./Footer";
 import Header from "./Header";
 import List from "./List";
+import Students from "./Students";
 
 function App() {
   return (
@@ -9,6 +10,10 @@ function App() {
     
     <Header/>
     <List/>
+    <Students name="Anush" age={28} isStudent="true"/>
+    <Students name="Ram" age={500} isStudent="false"/>
+    <Card/>
+    <Card/>
     <Card/>
     <Footer/>
     
---
+++ src/Students.jsx
@@ -0,0 +1,11 @@
+function Students({name="Guest",age="30",isStudent=false}){
+return(
+    <>
+    <p>Name:{name} </p>
+    <p>Age:{age} </p>
+    <p>Student:{isStudent} </p>
+    </>
+)
+}
+
+export default Students
