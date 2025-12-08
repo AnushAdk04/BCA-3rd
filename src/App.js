61d2b64746d37b4928ad44a51024aab60c2b52d2
AnushAdk04 <adhikarianush64@gmail.com>
2025-12-08 08:46:24 +0545

    counter app updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Card from "./Card";
+import Counter from "./Counter";
 import Footer from "./Footer";
 import Header from "./Header";
 import List from "./List";
@@ -7,16 +8,16 @@ import Students from "./Students";
 function App() {
   return (
     <>
-    
-    <Header/>
-    <List/>
-    <Students name="Anush" age={28} isStudent="true"/>
-    <Students name="Ram" age={500} isStudent="false"/>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Footer/>
-    
+      <Counter />
+      <Header />
+      <List />
+      <Students name="Anush" age={28} isStudent="true" />
+      <Students name="Ram" age={500} isStudent="false" />
+      <Card />
+      <Card />
+      <Card />
+      <Footer />
+
     </>
   );
 }
